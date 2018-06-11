98fb1c0891928dd907b4d6c56da719f6497d611c
edgar silva <esilva@thoughtworks.com>
2018-06-11 10:46:36 -0300

    Removing unused methods

--- app/src/main/java/workshop/com/models/place/PlaceDAO.kt
+++ app/src/main/java/workshop/com/models/place/PlaceDAO.kt
@@ -4,15 +4,6 @@ import androidx.room.*
 
 @Dao
 interface PlaceDAO {
-    @Query("Select * from Place")
-    fun getAll(): List<Place>
-
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     fun insert(place: Place)
-
-    @Insert(onConflict = OnConflictStrategy.REPLACE)
-    fun insert(vararg place: Place)
-
-    @Delete
-    fun delete(place: Place)
 }
