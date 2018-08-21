54e0eb86dcbb23f988627931daa9deab02e564ed
edgar silva <esilva@thoughtworks.com>
2018-08-21 00:12:04 -0300

    Finishing step 8, filtering with ReactiveX

--- app/src/main/java/workshop/com/views/MainActivity.kt
+++ app/src/main/java/workshop/com/views/MainActivity.kt
@@ -2,6 +2,9 @@ package workshop.com.views
 
 import android.content.Intent
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import io.reactivex.disposables.CompositeDisposable
@@ -12,6 +15,7 @@ import workshop.com.models.place.Place
 import workshop.com.views.place.PLaceFormActivity
 import workshop.com.views.place.PlaceAdapter
 import workshop.com.views.place.PlaceViewModel
+import java.util.*
 
 
 class MainActivity : AppCompatActivity() {
@@ -29,13 +33,29 @@ class MainActivity : AppCompatActivity() {
 
         setListeners()
         initRecyclerView()
-        loadPlaces()
+        loadPlaces("")
+    }
+
+    private fun TextView.onTextChange(textWatcher: (String) -> Unit) {
+        this.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(text: Editable?) {}
+
+            override fun beforeTextChanged(text: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+
+            override fun onTextChanged(text: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                textWatcher(text.toString())
+            }
+
+        })
     }
 
     private fun setListeners() {
         addButton.setOnClickListener {
             startActivityForResult(Intent(this, PLaceFormActivity::class.java), 200)
         }
+
+        searchEditText.onTextChange { loadPlaces(it) }
     }
 
     private fun initRecyclerView() {
@@ -43,12 +63,21 @@ class MainActivity : AppCompatActivity() {
         recyclerPlaceList.adapter = PlaceAdapter(placeList)
     }
 
-    private fun loadPlaces() {
-        disposable.add(placeViewModel.getAll().subscribe {
-            placeList.clear()
-            placeList.addAll(it)
-            recyclerPlaceList.adapter?.notifyDataSetChanged()
-        })
+    private fun loadPlaces(query: String) {
+        placeViewModel.getAll()
+                .doOnRequest { placeList.clear() }
+                .flatMapIterable { it }
+                .filter { placeFilter(it, query) }
+                .subscribe {
+                    placeList.add(it)
+                    recyclerPlaceList.adapter?.notifyDataSetChanged()
+                }
+    }
+
+    private fun placeFilter(place: Place, query: String): Boolean {
+        return query.isBlank()
+                || place.name.toUpperCase().contains(query.toUpperCase())
+                || place.description.toUpperCase().contains(query.toUpperCase())
     }
 
     override fun onDestroy() {
